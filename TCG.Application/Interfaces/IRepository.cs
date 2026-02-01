934e89f514b63de185298f455797c185b0ed3114
BenNichollsDev <98bennicholls@pm.me>
2026-02-01 19:44:46 +0000

    Repo functions added

--- TCG.Application/Interfaces/IRepository.cs
+++ TCG.Application/Interfaces/IRepository.cs
@@ -10,13 +10,13 @@ namespace TCG.Application.Interfaces
 {
     public interface IRepository
     {
-        Task<List<T>> GetAllAsync<T>() where T : class;
+        Task<List<T>?> GetAllAsync<T>() where T : class;
 
-        Task<T> GetByIdAsync<T>(int id) where T : class;
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
 
-        Task<T> GetByAsync<T>(Expression<Func<T, bool>> predicate) where T : class;
+        Task<T?> GetByAsync<T>(Expression<Func<T, bool>> predicate) where T : class;
 
-        Task<List<T>> GetAllByAsync<T>(Expression<Func<T, bool>> predicate) where T : class;
+        Task<List<T>?> GetAllByAsync<T>(Expression<Func<T, bool>> predicate) where T : class;
 
         Task<T> AddAsync<T>(T entity) where T : class;
 
--- tcg.infrastructure/Repository.cs
+++ tcg.infrastructure/Repository.cs
@@ -10,43 +10,56 @@ namespace TCG.Infrastructure
 {
     public class Repository : IRepository
     {
-        private readonly AppDbContext _db;
+        public readonly AppDbContext _db;
 
         //CAN I FIND A REFERENCE FOR ALL OF THESE???????
 
-        async Task<List<T>> GetAllAsync<T>() where T : class
+        public async Task<List<T>?> GetAllAsync<T>() where T : class
         {
-            return await _db.Set<T>().AsNoTracking().ToListAsync();
+            return await _db.Set<T>()
+                .AsNoTracking()
+                .ToListAsync();
         }
 
-        Task<T> GetByIdAsync<T>(int id) where T : class
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
         {
-
+            return await _db.Set<T>()
+                .FindAsync(id);
         }
 
-        Task<T> GetByAsync<T>(Expression<Func<T, bool>> predicate) where T : class
+        public async Task<T?> GetByAsync<T>(Expression<Func<T, bool>> predicate) where T : class
         {
-
+            return await _db.Set<T>()
+                .FirstOrDefaultAsync(predicate);
         }
 
-        Task<List<T>> GetAllByAsync<T>(Expression<Func<T, bool>> predicate) where T : class
+        public async Task<List<T>?> GetAllByAsync<T>(Expression<Func<T, bool>> predicate) where T : class
         {
-
+            return await _db.Set<T>()
+                .AsNoTracking()
+                .Where(predicate)
+                .ToListAsync();
         }
 
-        Task<T> AddAsync<T>(T entity) where T : class
+        public async Task<T> AddAsync<T>(T entity) where T : class
         {
-
+            _db.Set<T>().Add(entity);
+            await _db.SaveChangesAsync();
+            return entity;
         }
 
-        Task<T> UpdateAsync<T>(T entity) where T : class
+        public async Task<T> UpdateAsync<T>(T entity) where T : class
         {
-
+            _db.Set<T>().Update(entity);
+            await _db.SaveChangesAsync();
+            return entity;
         }
 
-        Task<T> DeleteAsync<T>(T entity) where T : class
+        public async Task<T> DeleteAsync<T>(T entity) where T : class
         {
-
+            _db.Set<T>().Remove(entity);
+            await _db.SaveChangesAsync();
+            return entity;
         }
     }
 }
