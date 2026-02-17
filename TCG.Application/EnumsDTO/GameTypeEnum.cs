0417cacdf2d02658defe8b6d4ae10553041b624f
BenNichollsDev <98bennicholls@pm.me>
2026-02-17 01:28:59 +0000

    Checkpoint after making game format logic - untested

---
+++ TCG.Application/EnumsDTO/GameTypeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCG.Application.Enums
+{
+    //public class GameTypeEnumDto
+    //{
+
+    //}
+
+    public enum GameTypeEnum
+    {
+        MTG,
+        Yugioh,
+        Pkmn
+    }
+}
