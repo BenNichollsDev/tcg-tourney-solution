63e1d888f84c7525e41974ca3e9cffb76c726321
BenNichollsDev <98bennicholls@pm.me>
2026-03-07 19:15:57 +0000

    Copied over template from tournaments

---
+++ TCG.Application/Interfaces/Services/ILeagueService.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using TCG.Application.Dtos;
+using TCG.Domain.Entities;
+
+namespace TCG.Application.Interfaces.Services;
+
+public interface ILeagueService
+{
+    Task<LeagueDto> GetByIdAsync(int leagueId);
+
+    Task<IEnumerable<LeagueDto>> GetAllAsync();
+
+    Task<LeagueDto> GetByAsync(Expression<Func<League, bool>> predicate);
+
+    Task<LeagueDto> CreateAsync(LeagueDto leagueDto);
+
+    Task<LeagueDto> UpdateAsync(LeagueDto leagueDto);
+
+    Task<LeagueDto> DeleteAsync(int id);
+}
---
+++ TCG.Infrastructure/Repositories/LeagueService.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using TCG.Application.Dtos;
+using TCG.Application.Interfaces;
+using TCG.Application.Interfaces.Services;
+using TCG.Domain.Entities;
+
+namespace TCG.Infrastructure.Repositories;
+
+public class LeagueService
+(
+    AppDbContext context,
+    IMapper mapper,
+    IUnitOfWork u
+)
+: ILeagueService
+{
+    public async Task<LeagueDto> GetByIdAsync(int leagueId)
+    {
+        var league = await context.Leagues
+            .AsNoTracking()
+            .Where(s => s.LeagueId == leagueId)
+            .ProjectTo<LeagueDto>(mapper.ConfigurationProvider)
+            .FirstOrDefaultAsync();
+
+        return league ?? throw new Exception($"League with id {leagueId} not found");
+    }
+
+    public async Task<IEnumerable<LeagueDto>> GetAllAsync()
+    {
+        return await context.Leagues
+            .AsNoTracking()
+            .ProjectTo<LeagueDto>(mapper.ConfigurationProvider)
+            .ToListAsync();
+    }
+
+    public async Task<LeagueDto> GetByAsync(Expression<Func<League, bool>> predicate)
+    {
+        var league = await context.Leagues
+            .AsNoTracking()
+            .Where(predicate)
+            .ProjectTo<LeagueDto>(mapper.ConfigurationProvider)
+            .FirstOrDefaultAsync();
+
+        return league ?? throw new Exception("League not found");
+    }
+
+    public async Task<LeagueDto> CreateAsync(LeagueDto leagueDto)
+    {
+        var league = mapper.Map<League>(leagueDto);
+
+        await context.Leagues.AddAsync(league);
+        await u.SaveChangesAsync();
+
+        return mapper.Map<LeagueDto>(league);
+    }
+
+    public async Task<LeagueDto> UpdateAsync(LeagueDto leagueDto)
+    {
+        var existingLeague = await context.Leagues
+            .FirstOrDefaultAsync(s => s.LeagueId == leagueDto.LeagueId);
+
+        if (existingLeague == null)
+            throw new Exception($"League with id {leagueDto.LeagueId} not found");
+
+        mapper.Map(leagueDto, existingLeague);
+
+        context.Leagues.Update(existingLeague);
+        await u.SaveChangesAsync();
+
+        return mapper.Map<LeagueDto>(existingLeague);
+    }
+
+    public async Task<LeagueDto> DeleteAsync(int id)
+    {
+        var league = await context.Leagues
+            .FirstOrDefaultAsync(s => s.LeagueId == id);
+
+        if (league == null)
+            throw new Exception($"League with id {id} not found");
+
+        context.Leagues.Remove(league);
+        await u.SaveChangesAsync();
+
+        return mapper.Map<LeagueDto>(league);
+    }
+}
