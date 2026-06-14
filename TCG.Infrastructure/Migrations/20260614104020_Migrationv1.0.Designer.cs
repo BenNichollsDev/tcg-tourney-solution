c939a5dd732e3c758093f71e4e3efc4532785581
BenNichollsDev <98bennicholls@pm.me>
2026-06-14 11:43:43 +0100

    Added final migration

--- TCG.Infrastructure/Migrations/20260614104020_Migrationv1.0.Designer.cs
+++ TCG.Infrastructure/Migrations/20260614104020_Migrationv1.0.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Numerics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TCG.Infrastructure;
@@ -12,9 +13,11 @@ using TCG.Infrastructure;
 namespace TCG.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260614104020_Migrationv1.0")]
+    partial class Migrationv10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TCG.Infrastructure/Migrations/20260614104020_Migrationv1.0.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TCG.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrationv10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "tp_bye",
+                table: "tournament_players");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "tp_bye",
+                table: "tournament_players",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
